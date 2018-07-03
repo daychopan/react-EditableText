260e80a0f54bd39a325e96df7d847af93b2d09e8
Jasim Ali <31367899+daychopan@users.noreply.github.com>
2018-07-03 19:41:13 +0200

    Probably final release
    cleaned up redundancy

--- index.js
+++ index.js
@@ -11,41 +11,30 @@ class EditableLabel extends Component {
     }
   }
 
-  changeToInput = () => {
+  changeEditable = () => {
     this.setState({
       editable: !this.state.editable
     })
   }
 
   handleChange = (e) => {
-    console.log(this.state.name)
-    console.log(e.target.value)
     this.setState({
       text: e.target.value
     })
   }
 
-  handleSubmit = (e) => {
-    console.log(this.state.text);
-    this.setState({
-      editable: !this.state.editable
-    });
-  }
-
   handleKeyPress = (e) => {
     if(e.key === 'Enter') {
-      this.setState({
-        editable: !this.state.editable
-      })
+      this.changeEditable()
     }
   }
 
   render() {
     if(!this.state.editable) {
-      return (<div onClick={this.changeToInput}>{this.state.text}</div>)
+      return (<div onClick={this.changeEditable}>{this.state.text}</div>)
     }
     else {
-      return (<div><input name={this.props.name} value={this.state.text} onBlur={this.handleSubmit} onKeyPress={this.handleKeyPress} onChange={this.handleChange} autoFocus/></div>);
+      return (<div><input name={this.props.name} value={this.state.text} onBlur={this.changeEditable} onKeyPress={this.handleKeyPress} onChange={this.handleChange} autoFocus/></div>);
     }
   }
 }
